f4ec83a0fa933079f826ecdf30c2c5fc54382b28
Pavlos Vinieratos <pvinis@gmail.com>
2020-07-27 10:24:36 +0200

    trying

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ const R = require("ramda")
 
 
 const hexName = (color) => hexNameOrig(color, {
-	white: "ffffff",
-	black: "000000",
-	yellow: "e39f02",
-	orange: "e35b02",
-	red: "d70f37",
-	cyan: "0f9bd7",
-	blue: "2141c6",
+	// background
+	"black": "000000",
+
+	// pieces
+	yellow: "e39f02", // square
+	cyan: "0f9bd7",   // line
+	red: "d70f37",    // Z
+	green: "59b101",  // S
+	orange: "e35b02", // L
+	blue: "2141c6",   // J
+
+	// wall
+	grey: "999999",
+
+	// piece shadows, while falling
+	syellow: "e39f02",
+	scyan: "0f9bd7",
+	sred: "d70f37",
+	sgreen: "2c5800",
+	sorange: "e35b02",
+	sblue: "2141c6",
+
   })
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
@@ -18,8 +33,20 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 const main = async () => {
 	const topLeftPosition = await readTopLeftPosition()
 
-	const grid = readGrid(topLeftPosition)
-	printGrid(grid)
+	let prevGrid
+	let grid
+	while (true) {
+		prevGrid = grid
+		grid = readGrid(topLeftPosition)
+		grid = processGrid(grid)
+
+		if (R.equals(prevGrid, grid)) {
+			// console.log("Skipping..")
+			continue
+		}
+
+		printGrid(grid)
+	}
 }
 
 
@@ -59,10 +86,22 @@ const readGrid = (topLeftPoint) => {
 	return grid
 }
 
+const processGrid = grid => {
+	R.map((row, irow) => {
+		R.map((square, icol) => {
+			if (square === 'black') {
+				console.log({irow, icol, grid})
+				// grid[irow][icol] = "wow"
+			}
+			console.log({square})
+		}, row)
+	}, grid)
+}
+
 const printGrid = grid => {
 	console.log("---Current Grid---")
-	grid.forEach(line => {
-		console.log(line.join("\t"))
+	grid.forEach(row => {
+		console.log(row.join("\t"))
 	})
 }
 
